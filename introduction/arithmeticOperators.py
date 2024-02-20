3b1c7cead840cab9a2945517b554ce0554dba63f
Gustavo Almeida <gustavogalvesalmeida@gmail.com>
2024-02-19 21:38:57 -0300

    feat: created operators for sum, division, multiplication and subtraction

---
+++ introduction/arithmeticOperators.py
@@ -0,0 +1,12 @@
+some_integer = 7
+
+result1 = 7 + 5
+result2 = some_integer - result1
+result3 = result2 * 3
+result4 = result3 / 4
+
+print("Sum result ", result1)
+print("Subtraction result", result2)
+print("Mutiplication result ", result3)
+print("Division result", result4)
+
