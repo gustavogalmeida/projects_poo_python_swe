ef73545da2e4e57717348855a0de57ffc7e0c754
Gustavo Almeida <gustavogalvesalmeida@gmail.com>
2024-02-19 21:21:30 -0300

    feat: declare and casting variables

---
+++ introduction/castingVariables.py
@@ -0,0 +1,12 @@
+some_float = 5.543
+
+print(some_float)
+print(type(some_float), "\n")
+
+some_float = int(some_float)
+print(some_float)
+print(type(some_float), "\n")
+
+some_float = str(some_float)
+print(some_float)
+print(type(some_float))
---
+++ introduction/main.py
@@ -0,0 +1,8 @@
+some_string = "Hello Word"
+some_int = 5
+some_float = 5.543
+some_boolean = True
+
+# print(some_string)
+print(type(some_boolean))
+
