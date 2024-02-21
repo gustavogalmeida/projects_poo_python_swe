43a4e4408d6926c8c03d3eeeaa78c93553a74c4b
Gustavo Almeida <gustavogalvesalmeida@gmail.com>
2024-02-20 21:57:48 -0300

    feat: include treatment with strings (indexing, substring, length, concatenation and multiplication

---
+++ introduction/strings.py
@@ -0,0 +1,26 @@
+# concatenation
+str_oo = "Object-oriented"
+str_p = "Programing"
+str_oop = str_oo + " " + str_p
+print(str_oop)
+
+# length
+print(len(str_oop))
+
+# indexing and substrings
+phrase = "Python is a powerful programming language"
+
+# the indexing start with 0
+print(phrase[3])
+
+# negative number starts at the end of the array
+print(phrase[-1])
+
+# using ":" i can inform [start:end]
+# the "end" is not included
+# example [0:6] is equal to [0:6)
+print(phrase[0:6])
+
+# multiplicating strings
+laughter = "ha"
+print(laughter*10)
