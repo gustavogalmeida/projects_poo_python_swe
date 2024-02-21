d2102dcabb9b646a85c6ff25f22b3974b5110f67
Gustavo Almeida <gustavogalvesalmeida@gmail.com>
2024-02-20 21:14:53 -0300

    feat: test equal operator

---
+++ introduction/equalOperator.py
@@ -0,0 +1,17 @@
+val_one = 1
+val_two = 2
+val_three = 3
+
+print(1 == val_one)
+print(3 == val_two)
+
+equal = val_one == val_two
+not_equal = val_one != val_two
+
+print(val_one, "is equal to", val_two, "=", equal)
+print(val_one, "is different to", val_two, "=", not_equal)
+
+print(val_one, "is greater than", val_two, "=", val_one>val_two)
+print(val_one, "is less than", val_two, "=", val_one<val_two)
+
+
