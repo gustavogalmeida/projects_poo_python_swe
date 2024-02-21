32cb80efd5d7688ea3b4e25855568fa6e4940ba0
Gustavo Almeida <gustavogalvesalmeida@gmail.com>
2024-02-20 22:30:02 -0300

    feat: implement (in, escaping, upper, lower, capitalize, format strings with %.

--- introduction/strings.py
+++ introduction/strings.py
@@ -24,3 +24,38 @@ print(phrase[0:6])
 # multiplicating strings
 laughter = "ha"
 print(laughter*10)
+
+# in operator
+phrase = "Python Programming"
+print("Python" in phrase)
+print("python" in phrase)
+print("lemon" in phrase)
+
+# escaping | break line
+print("Hello \nWorld!")
+
+# upper case and lower case convertions
+print(phrase)
+print(phrase.upper())
+print(phrase.lower())
+phrase = phrase.lower()
+print(phrase.capitalize()) # first latter capitalized
+
+print("\n")
+
+# format
+name = "Gustavo"
+int_number = 23
+float_numer = 3.1415
+
+print("Hello, my name is %s!\nI'm %d years old and $ %d.\n"
+      % (name, int_number, float_numer))
+# decimal printed as integer %d
+
+print("Hello, my name is %s!\nI'm %d years old and $ %f.\n"
+      % (name, int_number, float_numer))
+# decimal printed as decimal %f
+
+print("Hello, my name is %s!\nI'm %d years old and $ %.2f.\n"
+      % (name, int_number, float_numer))
+# decimal printed with two decimal places %.2f
