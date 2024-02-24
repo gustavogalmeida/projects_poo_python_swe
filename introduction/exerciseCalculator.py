0d1ee8da011d0b54b23fb5c8b59e97b891e71f80
Gustavo Almeida <gustavogalvesalmeida@gmail.com>
2024-02-24 09:28:24 -0300

    docs: challange description

---
+++ introduction/exerciseCalculator.py
@@ -0,0 +1,5 @@
+# Welcome function that greets the user and calls the calculate function.
+# The calculator function presents the available operations and prompts the user's choice.
+# It verifies if the chosen option is correct and performs the corresponding calculation.
+# Then, it asks the user if they want to calculate again, calling the calculate function again or ending the program.
+
