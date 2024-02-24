874fd229de61e7383439b91e4ffaf12cb1446466
Gustavo Almeida <gustavogalvesalmeida@gmail.com>
2024-02-24 09:27:44 -0300

    feat: this code calculate the avarage of 3 notes and verify if student was approved

---
+++ introduction/exerciseAvarage.py
@@ -0,0 +1,29 @@
+# First, we should ask for the person's name and give a welcome message.
+# Continue by collecting 3 grades and calculate the average.
+# If the average is >= 7, the student is approved.
+# If the average is < 3, the student is failed.
+# If 3 <= average < 7, the student goes to the final exam.
+
+name = str(input('Please enter your name: '))
+print('Wellcome, %s' %name,'!\n')
+
+print('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
+print('      The student %s, was approved?'%name)
+print('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
+
+note1 = float(input('Enter your first note: '))
+note2 = float(input('Enter your second note: '))
+note3 = float(input('Enter your third note: '))
+
+avarage = (note1 + note2 + note3)/3
+
+if avarage>7:
+    result = 'APPROVED'
+elif avarage<3:
+    result = 'DISAPPROVED'
+else:
+    result = 'FINAL EXAM'
+
+print('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
+print('      The result was: ', result)
+print('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
